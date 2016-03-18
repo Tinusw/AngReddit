2171391df07cd6142bc24a6e327e81c3d52b9c6f
Tinus <vertelmy@gmail.com>
2016-03-18 17:02:00 +0100

    added upvote a post feature

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ app.controller('MainCtrl', ['$scope',function($scope){
   ];
 
   $scope.addPost = function(){
+  	if(!$scope.title || $scope.title === '') { return;}
   	$scope.posts.push({title: $scope.title, upvotes: 0});
   	$scope.title= '';
   };
+
+	$scope.incrementUpvotes = function(post) {
+	  post.upvotes += 1;
+	};
 }]);
