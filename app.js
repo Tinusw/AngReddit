dd3bb8b2a8ac63e13bc6874617e6bea0c881351a
Tinus <vertelmy@gmail.com>
2016-03-18 17:44:07 +0100

    title=link(url) + bad styling

--- app.js
+++ app.js
@@ -6,17 +6,22 @@ app.controller('MainCtrl', ['$scope',function($scope){
   $scope.test = 'Hello world!';
 
   $scope.posts =[
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
+  {title: 'post 1', link: 'www.google.com', upvotes: 5},
+  {title: 'post 2', link: 'www.google.com', upvotes: 2},
+  {title: 'post 3', link: 'www.google.com', upvotes: 15},
+  {title: 'post 4', link: 'www.google.com', upvotes: 9},
+  {title: 'post 5', link: 'www.google.com', upvotes: 4}
   ];
 
   $scope.addPost = function(){
   	if(!$scope.title || $scope.title === '') { return;}
-  	$scope.posts.push({title: $scope.title, upvotes: 0});
+  	$scope.posts.push({
+  		title: $scope.title,
+  		link: $scope.link,
+  		upvotes: 0
+  	});
   	$scope.title= '';
+  	$scope.link = '';
   };
 
 	$scope.incrementUpvotes = function(post) {
